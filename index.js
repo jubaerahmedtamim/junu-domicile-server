7eb0dd9322ce1b723e50129ecb0ff70574d73d9b
jubaerahmedtamim <jubaerahmedtamim2014@gmail.com>
2025-01-12 19:48:31 +0600

    admin can delete user

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yatfw0u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -40,7 +40,16 @@ async function run() {
             }
             const result = await userCollection.insertOne(userInfo);
             res.send(result)
-            
+        })
+
+        app.get('/users', async(req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+        
+        app.delete('/users', async(req, res) => {
+            const result = await userCollection.deleteOne({_id: new ObjectId(req.query.id)})
+            res.send(result)
         })
 
 
