dda1700bc9242e7b9e567a99bbdd92c2d5ba2a39
jubaerahmedtamim <jubaerahmedtamim2014@gmail.com>
2025-01-14 02:56:39 +0600

    jwt & verifyToken middleware function done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -30,29 +31,52 @@ async function run() {
         const userCollection = client.db('JunuDomicileDB').collection('users');
 
 
+        // jwt related apis
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, { expiresIn: '1h' });
+            res.send({ token });
+        })
+
+        // middleware functions
+        const verifyToken = (req, res, next) => {
+            const bearerToken = req.headers.Authorization;
+            if (!bearerToken) {
+                return res.status(401).send({ message: 'unauthorized access' });
+            }
+            const token = bearerToken.split(' ')[1];
+            jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
         // users apis
         app.post('/users', async (req, res) => {
             const userInfo = req.body;
             const query = { email: userInfo.email }
             const isExisting = await userCollection.findOne(query);
-            if(isExisting){
-                return res.send({message: "User already exist.", insertedId: null})
+            if (isExisting) {
+                return res.send({ message: "User already exist.", insertedId: null })
             }
             const result = await userCollection.insertOne(userInfo);
             res.send(result)
         })
 
-        app.get('/users', async(req, res) => {
+        app.get('/users', async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
         })
-        
-        app.delete('/users', async(req, res) => {
-            const result = await userCollection.deleteOne({_id: new ObjectId(req.query.id)})
+
+        app.delete('/users', async (req, res) => {
+            const result = await userCollection.deleteOne({ _id: new ObjectId(req.query.id) })
             res.send(result)
         })
 
-        app.patch('/users', async(req, res)=> {
+        app.patch('/users', async (req, res) => {
             const id = req.query.id;
             const role = req.body;
             const filter = { _id: new ObjectId(id) };
